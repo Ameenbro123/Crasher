758a766a5d7a855c7971646a6a9c42ac4c474d3a
Ameenbro123 <81590743+Ameenbro123@users.noreply.github.com>
2021-09-20 21:37:18 -0700

    Update crash.py

--- crash.py
+++ crash.py
@@ -1,3 +1,6 @@
-import subprocess
-import os
-class crash:
+i=0
+while True:
+  with open("hi.txt","w") as f:
+    print(765464082653*i)
+    f.write("slap a turtle\n"*1000000)
+    i += (200455434*25*84565*65543^644*745)/25+i
